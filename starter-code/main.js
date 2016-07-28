431704c5318591075a5f27801ed3948bb1a40564
Paul Bryan <paulbryan2007@gmail.com>
2016-07-28 18:00:39 +0100

    Added if/else condition for match

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,13 @@
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.")
+} else if (cardThree === cardFour) {
+	alert("Sorry, try again.")
+} else if (cardOne === cardTwo) {
+	alert("You found a match!")
+}	
+
 var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
