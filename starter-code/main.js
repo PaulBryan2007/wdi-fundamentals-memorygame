fd2abe2468350026307e1898e2fa547ebb63d778
Paul Bryan <paulbryan2007@gmail.com>
2016-07-29 14:02:10 +0100

    general page facelift

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,14 @@
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("Sorry, try again.")
 } else if (cardThree === cardFour) {
 	alert("Sorry, try again.")
 } else if (cardOne === cardTwo) {
 	alert("You found a match!")
-}	
+}	*/
 
 var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
+var gameBoard = document.getElementById('game-board')
